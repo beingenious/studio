a7e9be1d89e74bdab6fa1f846eac3f873560420b
Benjamin Barbe <benjamin@pandasuite.com>
2024-04-15 17:28:49 +0200

    Electron builder integrate notarize

--- scripts/notarize.js
+++ scripts/notarize.js
@@ -1,5 +1,5 @@
 require('dotenv').config();
-const { notarize } = require('electron-notarize');
+const { notarize } = require('@electron/notarize');
 
 exports.default = async function notarizing(context) {
   const { electronPlatformName, appOutDir } = context;
@@ -10,10 +10,9 @@ exports.default = async function notarizing(context) {
   const appName = context.packager.appInfo.productFilename;
 
   await notarize({
-    appBundleId: 'com.pandasuite.studio',
-    appPath: `${appOutDir}/${appName}.app`,
     appleId: process.env.APPLE_ID,
     appleIdPassword: process.env.APPLE_ID_PASS,
-    ascProvider: process.env.APPLE_TEAM_ID,
+    teamId: process.env.APPLE_TEAM_ID,
+    appPath: `${appOutDir}/${appName}.app`,
   });
 };
